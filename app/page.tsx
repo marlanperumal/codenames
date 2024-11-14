ca6735ebcbd268f80eb11d483c8128ec2a92e802
Marlan Perumal <marlan.perumal@gmail.com>
2024-11-14 17:41:47 +0200

    Integrated supabase, shadcn and pnpm

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import ThemeSelector from "@/components/ui/theme-selector";
 
 export default function Home() {
   return (
@@ -95,6 +96,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <ThemeSelector />
       </footer>
     </div>
   );
